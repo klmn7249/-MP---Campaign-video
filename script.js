679fd856eef0cf0f507142288f3137dbd6286a59
klmn7249 <kelemeng771@gmail.com>
2026-03-31 13:59:18 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+const video = document.getElementById("video");
+const playBtn = document.getElementById("playBtn");
+const overlay = document.getElementById("overlay");
+const replayBtn = document.getElementById("replayBtn");
+
+/* PLAY (hanggal) */
+playBtn.addEventListener("click", () => {
+    video.muted = false;
+    video.volume = 1;
+
+    video.play();
+
+    /* overlay fade out */
+    overlay.style.opacity = "0";
+    setTimeout(() => {
+        overlay.style.display = "none";
+    }, 500);
+});
+
+/* vége */
+video.addEventListener("ended", () => {
+    replayBtn.style.display = "flex";
+});
+
+/* replay */
+replayBtn.addEventListener("click", () => {
+    video.currentTime = 0;
+    video.play();
+    replayBtn.style.display = "none";
+});
+
+/* tiltások */
+document.addEventListener("contextmenu", e => e.preventDefault());
+document.addEventListener("keydown", e => e.preventDefault());
